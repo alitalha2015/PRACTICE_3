aaabbb48d4ffcdfef49767c4b730e7e8fb48b689
alitalha2015 <meltemozdincer@gmail.com>
2021-02-09 23:33:54 +0000

    PROJE

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,7 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Meltem");
         System.out.println("meryem");
-
+        System.out.println("EU4");
 
         //commit
 
