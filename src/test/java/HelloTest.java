33a0229611c3d938bbe43a908accbbd1d96e2a5b
alitalha2015 <meltemozdincer@gmail.com>
2021-02-09 23:28:11 +0000

    clear

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,13 @@
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("Meltem");
+        System.out.println("meryem");
+
+
+        //commit
+
+        //proje
+        //proje
+    }
+
+}
